cd6038450272411337d87c122c163a9e45090ce5
Ivan Franchin <ivan.franchin@takeaway.com>
2022-01-06 14:02:07 -0300

    project update - update to spring-boot 2.6.2; - update to spring-native 0.11.1; - update to native-buildtools 0.9.9; - update to springdoc-openapi 1.6.4; - add CorsFilter bean as it is needed for Native app; - update README.

---
+++ simple-service/src/main/java/com/mycompany/simpleservice/config/CorsConfig.java
@@ -0,0 +1,28 @@
+package com.mycompany.simpleservice.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.Collections;
+import java.util.List;
+
+@Configuration
+public class CorsConfig {
+
+    // The CorsFilter bean definition is needed for Native app
+    @Bean
+    CorsFilter corsFilter(@Value("${app.cors.allowed-origins}") List<String> allowedOrigins) {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.setAllowedOrigins(allowedOrigins);
+        config.setAllowedMethods(Collections.singletonList("*"));
+        config.setAllowedHeaders(Collections.singletonList("*"));
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
+}
