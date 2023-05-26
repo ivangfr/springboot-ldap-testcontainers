29db4fecb98ed875e129d6806015b3aa8505dfe8
Ivan Franchin <ivan.franchin@takeaway.com>
2023-05-26 07:14:35 -0300

    Project upgrade, refactor WebSecurityConfig and code related to Testcontainers - upgrade to spring-boot 3.1.0; - upgrade to springdoc-openapi 2.1.0; - upgrade to mapstruct 1.5.5.Final. - upgrade to jib 3.3.2.

--- simple-service/src/main/java/com/ivanfranchin/simpleservice/security/WebSecurityConfig.java
+++ simple-service/src/main/java/com/ivanfranchin/simpleservice/security/WebSecurityConfig.java
@@ -3,8 +3,10 @@ package com.ivanfranchin.simpleservice.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -14,16 +16,18 @@ public class WebSecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests()
-                .requestMatchers(HttpMethod.GET, "/api/private").authenticated()
-                .requestMatchers(HttpMethod.GET, "/api/public").permitAll()
-                .requestMatchers(HttpMethod.GET, "/actuator/**").permitAll()
-                .requestMatchers("/swagger-ui.html", "/swagger-ui/**", "/v3/api-docs", "/v3/api-docs/**").permitAll()
-                .anyRequest().authenticated()
-                .and()
-                .httpBasic();
-        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-        http.cors().and().csrf().disable();
-        return http.build();
+        return http.authorizeHttpRequests(
+                        authorizeHttpRequests -> authorizeHttpRequests
+                                .requestMatchers(HttpMethod.GET, "/api/private").authenticated()
+                                .requestMatchers(HttpMethod.GET, "/api/public").permitAll()
+                                .requestMatchers(HttpMethod.GET, "/actuator/**").permitAll()
+                                .requestMatchers("/swagger-ui.html", "/swagger-ui/**", "/v3/api-docs", "/v3/api-docs/**").permitAll()
+                                .anyRequest().authenticated()
+                )
+                .httpBasic(Customizer.withDefaults())
+                .sessionManagement(sessionManagement -> sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .csrf(AbstractHttpConfigurer::disable)
+                .cors(AbstractHttpConfigurer::disable)
+                .build();
     }
 }
